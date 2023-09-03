b56f6c9be9282e90c70c6ce87972cdc6b230c39b
Manojkv31 <127300092+Manojkv31@users.noreply.github.com>
2023-09-03 10:43:37 +0530

    Number Management system

--- Number Management/src/App.js
+++ Number Management/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [newNumber, setNewNumber] = useState('');
+  const [editNumber, setEditNumber] = useState('');
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const addNumber = () => {
+    if (newNumber !== '') {
+      setNumbers([...numbers, newNumber]);
+      setNewNumber('');
+    }
+  };
+
+  const editNumberAtIndex = (index) => {
+    setEditNumber(numbers[index]);
+    setEditIndex(index);
+  };
+
+  const saveEditedNumber = () => {
+    if (editIndex !== -1) {
+      const updatedNumbers = [...numbers];
+      updatedNumbers[editIndex] = editNumber;
+      setNumbers(updatedNumbers);
+      setEditNumber('');
+      setEditIndex(-1);
+    }
+  };
+
+  const deleteNumberAtIndex = (index) => {
+    const updatedNumbers = [...numbers];
+    updatedNumbers.splice(index, 1);
+    setNumbers(updatedNumbers);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number Management System</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a number"
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
+        />
+        <button onClick={addNumber}>Add Number</button>
+      </div>
+      <ul>
+        {numbers.map((number, index) => (
+          <li key={index}>
+            {index === editIndex ? (
+              <>
+                <input
+                  type="text"
+                  value={editNumber}
+                  onChange={(e) => setEditNumber(e.target.value)}
+                />
+                <button onClick={saveEditedNumber}>Save</button>
+              </>
+            ) : (
+              <>
+                {number}
+                <button onClick={() => editNumberAtIndex(index)}>Edit</button>
+                <button onClick={() => deleteNumberAtIndex(index)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
